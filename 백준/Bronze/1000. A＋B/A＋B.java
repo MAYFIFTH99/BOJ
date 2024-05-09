6f81583200e079180a485fd0eedf8e9c6139eddc
Harvey <149806254+MAYFIFTH99@users.noreply.github.com>
2024-05-09 12:25:28 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14176 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,12 +1,22 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int A = scanner.nextInt();
-        int B = scanner.nextInt();
 
-        System.out.println(A+B);
+    public static void main(String[] args) throws IOException {
+        //Scanner 보단 BufferedReader 가 성능이 좋다
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String str = bufferedReader.readLine();
+
+        //split 보단 StringTokenizer 가 성능이 좋다.
+        StringTokenizer stringTokenizer = new StringTokenizer(str, " ");
+        //Integer.valueOf 보단 parseInt 를 사용하는 것이 좋다.
+        int a = Integer.parseInt(stringTokenizer.nextToken());
+        int b = Integer.parseInt(stringTokenizer.nextToken());
+
+        System.out.println(a+b);
     }
 
 }
