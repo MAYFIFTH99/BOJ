b113c19f617834fa73dee2e76924d036664cda76
Harvey <149806254+MAYFIFTH99@users.noreply.github.com>
2024-05-15 01:38:39 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[][] arr = new int[100][100];
+
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            for (int j = x; j < x+10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    arr[j][k] = 1;
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (arr[i][j] == 1) {
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
