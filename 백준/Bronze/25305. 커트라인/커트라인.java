fb95ef9325a5d976ac7476e168ffc4889905f266
Harvey <149806254+MAYFIFTH99@users.noreply.github.com>
2024-05-19 23:18:40 +0900

    [Bronze II] Title: 커트라인, Time: 276 ms, Memory: 20712 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int k = scanner.nextInt();
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[N-k]);
+
+
+
+    }
+}
+
