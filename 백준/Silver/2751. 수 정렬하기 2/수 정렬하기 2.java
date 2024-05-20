f44c70a5bcc3ca6707aec110b02955efacc3b820
Harvey <149806254+MAYFIFTH99@users.noreply.github.com>
2024-05-20 11:36:54 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 744 ms, Memory: 93952 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,30 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-
-
-        Scanner in = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
-
-        int N = in.nextInt();
-
-        ArrayList<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i < N; i++) {
-            list.add(in.nextInt());
-        }
-
-        Collections.sort(list);
-
-        for (int value : list) {
-            sb.append(value).append('\n');
-        }
-        System.out.println(sb);
+	public static void main(String[] args) throws IOException {
+    
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+        
+		/*
+		  -1000000 ~ 1000000
+		  기준점 0 = index 100000 으로 생각
+		*/
+		boolean[] arr = new boolean[2000001];	
+        
+		int N = Integer.parseInt(br.readLine());
+        
+		for(int i = 0; i < N; i++) {
+			arr[Integer.parseInt(br.readLine()) + 1000000] = true;
+		}
+ 
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i]) {
+				sb.append((i - 1000000)).append('\n');
+			}
+		}
+		System.out.print(sb);
+	}
 }
-}
-
-
