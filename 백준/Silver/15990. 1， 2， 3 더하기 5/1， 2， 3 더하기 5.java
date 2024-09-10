e2dfc34c27f61994416594709883acc68e033cd8
Harvey <149806254+MAYFIFTH99@users.noreply.github.com>
2024-09-10 11:10:48 +0900

    [Silver II] Title: 1, 2, 3 더하기 5, Time: 136 ms, Memory: 21264 KB -BaekjoonHub

--- 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
+++ 백준/Silver/15990. 1， 2， 3 더하기 5/1， 2， 3 더하기 5.java
@@ -1,36 +1,39 @@
-import java.io.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * 1,2,3 더하기 5
+ * 실버 2
+ * 31%
+ */
 public class Main {
 
-	static final int MOD = 1000000009;
-	static long[][] dp = new long[100001][4];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
 
-	public static void main(String[] args) throws Exception {
+        final int MOD = 1_000_000_009;
+        long[][] dp = new long[100_001][4];
+        dp[1][1] = 1;
+        dp[2][2] = 1;
+        dp[3][1] = 1;
+        dp[3][2] = 1;
+        dp[3][3] = 1;
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int j = 4 ; j <= 100_000; j++) {
+            dp[j][1] = (dp[j - 1][2] + dp[j - 1][3]) % MOD;
+            dp[j][2] = (dp[j - 2][1] + dp[j - 2][3]) % MOD;
+            dp[j][3] = (dp[j - 3][1] + dp[j - 3][2]) % MOD;
+        }
 
-		dp[1][1] = 1;
-		dp[2][2] = 1;
-		dp[3][1] = 1;
-		dp[3][2] = 1;
-		dp[3][3] = 1;
+        for (int i = 0; i < T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            sb.append((dp[n][1] + dp[n][2] + dp[n][3]) % MOD).append("\n");
+        }
 
-		for (int i = 4; i <= 100000; i++) {
-			dp[i][1] = (dp[i - 1][2] + dp[i - 1][3]) % MOD;
-			dp[i][2] = (dp[i - 2][1] + dp[i - 2][3]) % MOD;
-			dp[i][3] = (dp[i - 3][1] + dp[i - 3][2]) % MOD;
-		}
-
-		int T = Integer.parseInt(br.readLine());
-
-		for (int t = 0; t < T; t++) {
-			int n = Integer.parseInt(br.readLine());
-			long res = (dp[n][1] + dp[n][2] + dp[n][3]) % MOD;
-			bw.write(res + "\n");
-		}
-
-		bw.flush();
-	}
-
+        System.out.println(sb);
+    }
 }
