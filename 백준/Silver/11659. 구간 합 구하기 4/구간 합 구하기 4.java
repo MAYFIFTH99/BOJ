af961611a1fc4664c45f9e271b12bc7594124359
Harvey <149806254+MAYFIFTH99@users.noreply.github.com>
2024-09-24 11:18:51 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1176 ms, Memory: 64320 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,39 +1,38 @@
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    // 구간 합 구하기
-    // 실버3 11659번
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int[] numbers = new int[N];
-        for (int i = 0; i < N; i++) {
-            numbers[i] = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] sequence = new int[N+1];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            sequence[i] = Integer.parseInt(st.nextToken());
         }
-        int[] newNumbers = sectionSum(numbers, N);
+
+        int[] sum = new int[N+1];
+        sum[0] = 0;
+        sum[1] = sequence[1];
+
+        for (int i = 2; i <= N; i++) {
+            sum[i] = sum[i-1] + sequence[i];
+        }
+
         for (int i = 0; i < M; i++) {
-            int A = sc.nextInt();
-            int B = sc.nextInt();
-            // A 부터 B까지의 구간 합
-            if (A == 1) {
-                System.out.println(newNumbers[B - 1]);
-            } else {
-
-                System.out.println(newNumbers[B-1] - newNumbers[A-2]);
-            }
-
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            System.out.println(sum[end] - sum[start-1]);
         }
-    }
-
-    public static int[] sectionSum(int[] arr, int N) {
-        int[] sectionSum = new int[N];
-        sectionSum[0] = arr[0];
-        for (int i = 1; i < sectionSum.length; i++) {
-            sectionSum[i] = sectionSum[i-1] + arr[i];
-        }
-
-        return sectionSum;
     }
 }
