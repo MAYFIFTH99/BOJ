e3106afdfef5e81fd539ae6fbb7991f553d7cd9d
Harvey <149806254+MAYFIFTH99@users.noreply.github.com>
2024-09-27 14:43:07 +0900

    [Silver IV] Title: 카드2, Time: 156 ms, Memory: 28944 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,34 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/**
+ * 실버 4
+ * 2164번
+ * 카드 게임
+ * 큐 이용
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 1; i <= N; i++) {
+            queue.offer(i);
+        }
+
+        while (queue.size() != 1) {
+            queue.poll();
+            queue.offer(queue.poll());
+        }
+
+        System.out.println(queue.peek());
+
+    }
+}
