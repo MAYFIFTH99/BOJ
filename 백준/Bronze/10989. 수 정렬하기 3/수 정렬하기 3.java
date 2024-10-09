5d836b780d76fef2a446173a67ae5196c432f947
Harvey <149806254+MAYFIFTH99@users.noreply.github.com>
2024-10-09 22:16:02 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1532 ms, Memory: 336796 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,31 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
 
+import java.io.*;
+
+/**
+ * 계수, 기수 정렬 사용해보기
+ *
+ */
 public class Main {
     public static void main(String[] args) throws IOException {
 
-
-        int[] cnt = new int[10001];
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int N = Integer.parseInt(br.readLine());
 
+        int[] A = new int[10001];
+
         for (int i = 0; i < N; i++) {
-            cnt[Integer.parseInt(br.readLine())]++;
+            A[Integer.parseInt(br.readLine())]++;
         }
 
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 1; i < cnt.length; i++) {
-            while (cnt[i] > 0) {
-                sb.append(i).append('\n');
-                cnt[i]--;
+        for (int i = 1; i < A.length; i++) {
+            while (A[i] > 0) {
+                sb.append(i).append("\n");
+                A[i]--;
             }
         }
         System.out.println(sb);
-}
+    }
 }
 
 
+
+/**
+ * 기수 정렬
+ * public static void radixSort(int len) {
+ *         Queue<Integer>[] bucket = new LinkedList[BUCKET_SIZE];
+ *         for(int i=0; i<BUCKET_SIZE; i++) {
+ *             bucket[i] = new LinkedList<>();
+ *         }
+ *
+ *         // 정렬할 자릿수의 크기 만큼 반복한다.
+ *         // 현재 배열의 가장 큰 수는 10의 자리이므로, 2번 반복하게 한다.
+ *         int m = 1;
+ *         for(int d=0; d<2; d++) {
+ *             for(int i=0; i<len; i++) {
+ *                 bucket[(A[i]/m)%10].add(A[i]);
+ *             }
+ *
+ *             for(int i=0, j=0; i<BUCKET_SIZE; i++) {
+ *                 while(!bucket[i].isEmpty()) {
+ *                     A[j++] = bucket[i].poll();
+ *                 }
+ *             }
+ *             m *= 10;
+ *         }
+ *     }
+ */
