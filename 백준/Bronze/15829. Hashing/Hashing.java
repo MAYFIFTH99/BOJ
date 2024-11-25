8aaa64eb85be41b9d3942d3548258fe6b715ff23
Harvey <149806254+MAYFIFTH99@users.noreply.github.com>
2024-11-25 21:46:40 +0900

    [Bronze II] Title: Hashing, Time: 172 ms, Memory: 17740 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,31 +1,34 @@
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
-/**
- * 브론즈 2
- * 30%
- */
 public class Main {
-    static int r = 31;
-    static int M = 1234567891;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int len = Integer.parseInt(br.readLine());
-        String str = br.readLine();
+    public static void main(String[] args) {
+        final int r = 31;  // 31
+        final int M = 1234567891;  // 1234567891
 
-        char[] charArray = str.toCharArray();
-        int[] intArray = new int[len];
+        // 변수 선언
+        int len;
+        String str;
+        int alphaIndex;
+        long powValue;
+        long sum = 0;
 
-        for (int i = 0; i < len; i++) {
-            intArray[i] = (int)charArray[i] - 96;
+        // 입력받기
+        Scanner sc = new Scanner(System.in);
+        len = sc.nextInt();     // 길이
+        str = sc.next();        // 알파벳 string
+
+        String[] strArray = str.split("");      // 입력받은 알파벳 String을 한글자씩 쪼개줘서 담아놓기
+
+        powValue = (long) Math.pow(r, 0);   // == 1
+
+        for(int i=0; i<len; i++) {
+            alphaIndex = (int)(strArray[i].charAt(0))-96;   // 알파벳의 순서 번호
+            sum += alphaIndex * powValue % M;
+            powValue = (powValue * r) % M;  // 이전 r의 0승 값에다가 31을 계속 누적으로 곱해줘서 저장 (M을 나누는건 범위 안에 들어오게하기 위해)
         }
 
-        int sum = 0;
-        for (int i = 0; i < len; i++) {
-            sum += (intArray[i] * Math.pow(r, i)) % M;
-        }
+        sum = sum % M;      // 모든 계산 이후 mod M 수행까지 해야 진짜 정답
         System.out.println(sum);
     }
 }
