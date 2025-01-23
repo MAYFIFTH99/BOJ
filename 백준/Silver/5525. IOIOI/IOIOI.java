32139820a55e76e053dd8e34b8fa72c4a4d6a8ea
Harvey <149806254+MAYFIFTH99@users.noreply.github.com>
2025-01-23 13:17:41 +0900

    [Silver I] Title: IOIOI, Time: 252 ms, Memory: 20076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -1,47 +1,47 @@
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-// 29% Silver1
 public class Main {
-    static StringBuilder sb;
-    static String P;
-    static String S;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sb = new StringBuilder();
+
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        S = br.readLine();
+        String str = br.readLine();
 
-        sb.append("I");
-        for (int i = 1; i <= N; i++) {
-            sb.append("OI");
-        }
-
-        P = sb.toString().trim();
-
-        int start = 0;
-        int end = P.length() - 1;
-        int count = 0;
-        while (start <= S.length() - sb.length()) {
-
-            if (extractStr(start, end)) {
-                count++;
+        int answer = 0;
+        for(int i=0; i<M; i++) {
+            // I를 찾았으면, 패턴 체크
+            if(str.charAt(i) == 'I') {
+                // 문자열 탐색을 위한 변수 선언
+                int tmp = i;
+                // 패턴 파악을 위한 변수 선언
+                int cnt = 0;
+                
+                // 인덱스를 벗어나지 않을 때까지 탐색
+                while(tmp+2 < M) {
+                    // IOI 패턴인지 체크
+                    if(str.charAt(tmp+1) == 'O' && str.charAt(tmp+2) == 'I') {
+                        // 맞으면 cnt+1 패턴을 파악한 셈
+                        cnt++;
+                        // 다음 오른쪽으로 이동
+                        tmp += 2;
+                        
+                        // Pn 패턴이 맞으면 정답에 추가
+                        // 겹쳐진 패턴 탐색을 위해 다시 - 1
+                        if(cnt == N) {
+                            cnt--;
+                            answer++;
+                        }
+                    }
+                    else {
+                        break;
+                    }
+                }
+                // 마지막 탐색 인덱스로 이동
+                i = tmp;
             }
-            start++;
-            end++;
         }
-
-        System.out.println(count);
-
-
-
+        System.out.println(answer);
     }
-
-    private static boolean extractStr(int start, int end) {
-        return S.substring(start, end+1).equals(P);
-    }
-
 }
