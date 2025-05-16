c650a9e87151173539a91de598d08a9046f3bf6b
Harvey <149806254+MAYFIFTH99@users.noreply.github.com>
2025-05-16 15:53:19 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 792 ms, Memory: 196740 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -6,29 +6,29 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N;
-    static int[] arr;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        int[] have = new int[20000001];
 
-        int[] arr = new int[20000001];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[Integer.parseInt(st.nextToken()) + 10000000]++;
+            have[10000000 + Integer.parseInt(st.nextToken())]++;
         }
 
         int M = Integer.parseInt(br.readLine());
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            sb.append(arr[Integer.parseInt(st.nextToken()) + 10000000]);
-            sb.append(" ");
+            sb.append(have[10000000 + Integer.parseInt(st.nextToken())]).append(" ");
         }
 
-        System.out.println(sb);
+        String result = sb.substring(0, sb.length() - 1);
+        System.out.println(result);
+
     }
 
 }
+
