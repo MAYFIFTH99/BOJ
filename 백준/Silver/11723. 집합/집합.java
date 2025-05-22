99096c9cec38801a736f747ab6cab4c083a18d38
Harvey <149806254+MAYFIFTH99@users.noreply.github.com>
2025-05-23 07:29:47 +0900

    [Silver V] Title: 집합, Time: 1296 ms, Memory: 312264 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,65 +1,94 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static LinkedList<Integer> list = new LinkedList<>();
+    static StringBuilder sb = new StringBuilder();
+    static LinkedList<Integer> allList = new LinkedList<>();
+    static {
+        for (int i = 1; i <= 20; i++) {
+            allList.add(i);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
+
+        list = new LinkedList<>();
+
         int M = Integer.parseInt(br.readLine());
 
-        Set<Integer> set = new HashSet<>();
-
         for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine());
-            String inst = st.nextToken();
-            switch (inst) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            switch (st.nextToken()) {
                 case "add":
                     int x = Integer.parseInt(st.nextToken());
-                    set.add(x);
+                    add(x);
                     break;
                 case "remove":
                     int y = Integer.parseInt(st.nextToken());
-                    try {
-                        set.remove(y);
-                    } catch (Exception e) {
-                        break;
-                    }
+                    remove(y);
                     break;
                 case "check":
                     int z = Integer.parseInt(st.nextToken());
-                    if (set.contains(z)) {
-                        sb.append("1").append("\n");
-                    } else {
-                        sb.append("0").append("\n");
-                    }
+                    check(z);
                     break;
                 case "toggle":
-                    int k = Integer.parseInt(st.nextToken());
-                    if (set.contains(k)) {
-                        set.remove(k);
-                    } else {
-                        set.add(k);
-                    }
-
+                    int w = Integer.parseInt(st.nextToken());
+                    toggle(w);
                     break;
-
                 case "all":
-                    set.clear();
-                    for (int j = 1; j <= 20; j++) {
-                        set.add(j);
-                    }
+                    all();
                     break;
                 case "empty":
-                    set.clear();
+                    empty();
                     break;
+
             }
         }
         System.out.println(sb);
 
+    }
+
+    private static void all() {
+        list = new LinkedList<>(allList);
+    }
+
+    private static void empty() {
+        list.clear();
+    }
+
+    private static void toggle(int w) {
+        if (list.contains(w)) {
+            list.remove((Object) w);
+        } else {
+            list.add(w);
+        }
+    }
+
+    private static void check(int z) {
+        if (list.contains(z)) {
+            sb.append("1\n");
+        } else {
+            sb.append("0\n");
+        }
+    }
+
+    private static void remove(int y) {
+        if (list.contains(y)) {
+            list.remove((Object) y);
+        }
+    }
+
+    private static void add(int x) {
+        if (!list.contains(x)) {
+            list.add(x);
+        }
     }
 }
