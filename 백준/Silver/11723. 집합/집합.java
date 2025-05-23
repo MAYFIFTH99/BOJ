16534bf53b86e2da343c75433a490abb8a490602
Harvey <149806254+MAYFIFTH99@users.noreply.github.com>
2025-05-23 09:03:34 +0900

    [Silver V] Title: 집합, Time: 1168 ms, Memory: 313212 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -2,93 +2,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static LinkedList<Integer> list = new LinkedList<>();
-    static StringBuilder sb = new StringBuilder();
-    static LinkedList<Integer> allList = new LinkedList<>();
-    static {
-        for (int i = 1; i <= 20; i++) {
-            allList.add(i);
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        list = new LinkedList<>();
-
+        int S = 0;
         int M = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < M; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
+        while (M-- > 0) {
 
-            switch (st.nextToken()) {
-                case "add":
-                    int x = Integer.parseInt(st.nextToken());
-                    add(x);
-                    break;
-                case "remove":
-                    int y = Integer.parseInt(st.nextToken());
-                    remove(y);
-                    break;
-                case "check":
-                    int z = Integer.parseInt(st.nextToken());
-                    check(z);
-                    break;
-                case "toggle":
-                    int w = Integer.parseInt(st.nextToken());
-                    toggle(w);
-                    break;
-                case "all":
-                    all();
-                    break;
-                case "empty":
-                    empty();
-                    break;
+            st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();
 
+            if(str.equals("all")) S = (1 << 21) - 1;
+
+            else if(str.equals("empty")) S = 0;
+
+            else {
+                int num = Integer.parseInt(st.nextToken());
+
+                if(str.equals("add"))
+                    S |= (1 << num);
+                else if(str.equals("remove"))
+                    S &= ~(1 << num);
+                else if(str.equals("check"))
+                    sb.append((S & (1 << num)) != 0 ? 1 : 0).append("\n");
+                else if(str.equals("toggle"))
+                    S ^= (1 << num);
             }
         }
+
         System.out.println(sb);
-
-    }
-
-    private static void all() {
-        list = new LinkedList<>(allList);
-    }
-
-    private static void empty() {
-        list.clear();
-    }
-
-    private static void toggle(int w) {
-        if (list.contains(w)) {
-            list.remove((Object) w);
-        } else {
-            list.add(w);
-        }
-    }
-
-    private static void check(int z) {
-        if (list.contains(z)) {
-            sb.append("1\n");
-        } else {
-            sb.append("0\n");
-        }
-    }
-
-    private static void remove(int y) {
-        if (list.contains(y)) {
-            list.remove((Object) y);
-        }
-    }
-
-    private static void add(int x) {
-        if (!list.contains(x)) {
-            list.add(x);
-        }
     }
 }
